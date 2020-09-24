b8300203714544af70b3ed2264221be16550dc0e
kmr614 <kmr614@gmail.com>
2020-09-24 18:59:56 -0500

    Testing an update

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import sys
+import numpy as np
 
 
 def test():
     print("This is a test")
+    Print("This is an addition")
+    Print("I hope this works")
 
 test_array = [[1,2,3], [4,5,6], [7,8,9]]
 ##list_as_array = np.array(test_array)
