2577ed30a210db75ae3ff4e8175ad5d098d77556
Krishna Ramesh <kmr614@gmail.com>
2020-09-24 21:55:39 -0500

    another commit

--- test.py
+++ test.py
@@ -7,11 +7,7 @@ def test():
     print("trying this from visual studios")
 
 test_array = [[1,2,3], [4,5,6], [7,8,9]]
-##list_as_array = np.array(test_array)
-
-#this is new comment
-#now i am going to make the code change in vs then push through cmd
-
+list_as_array = np.array(test_array)
 
 
 
