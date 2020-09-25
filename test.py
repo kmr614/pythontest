1656d0932156dc3d0dc55e9f66a35b27376d0044
kmr614 <kmr614@gmail.com>
2020-09-24 21:54:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 import sys
 import numpy as np
 
-
+int x = 10
 def test():
    
     print("trying this from visual studios")
 
 test_array = [[1,2,3], [4,5,6], [7,8,9]]
-##list_as_array = np.array(test_array)
+list_as_array = np.array(test_array)
 
-#this is new comment
-#now i am going to make the code change in vs then push through cmd
+
 
 
 
