63039f126f93996fe634a942c3148bea92d18f78
Krishna Ramesh <kmr614@gmail.com>
2020-09-24 19:14:54 -0500

    test commit from vs code 1

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import numpy as np
 
 def test():
     print("This is a test")
-    Print("This is an addition")
-    Print("I hope this works")
+    print("This is an addition")
+    print("I hope this works")
+    print("trying this from vs code")
 
 test_array = [[1,2,3], [4,5,6], [7,8,9]]
 ##list_as_array = np.array(test_array)
