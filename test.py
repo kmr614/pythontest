2a743391ef76bad0af4504b704749b411188a565
Krishna Ramesh <kmr614@gmail.com>
2020-09-24 19:47:15 -0500

    new comment

--- test.py
+++ test.py
@@ -3,16 +3,14 @@ import numpy as np
 
 
 def test():
-    print("This is a test")
-    print("This is an addition")
-    print("I hope this works")
-    print("trying this from vs code")
+   
+    print("trying this from visual studios")
 
 test_array = [[1,2,3], [4,5,6], [7,8,9]]
 ##list_as_array = np.array(test_array)
 
+#this is new comment
 
-print("This the second commet")
 
 
 
